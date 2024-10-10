21705c392309513a94290d0ba7a50124c3a49665
Otto Ventura <139597154+OTEKE10@users.noreply.github.com>
2024-10-09 19:58:09 -0600

    cambios

--- elcajonniccos/includes/config/database.php
+++ elcajonniccos/includes/config/database.php
@@ -1,7 +1,9 @@
 <?php
  
 function conectarDB() : mysqli {
+    //$db = mysqli_connect('sql205.infinityfree.com', 'if0_37481964', 'QLHe4hWyB6', 'if0_37481964_cajoniccos');
     $db = mysqli_connect('localhost', 'root', 'wolverin10', 'cajondeniccos_crud');
+    
     $db->set_charset("utf8");
  
     if(!$db) {
